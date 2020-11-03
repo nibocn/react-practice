049ddaf01652a4c72dfcd201daac751aa3076910
NiBo <nibocn@gmail.com>
2020-11-03 17:25:17 +0800

    feat: React 集成 TypeScript

---
+++ 03_hello-react-ts/src/components/Hello.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Hello.css'
+
+export interface Props {
+  name: string
+  enthusiasmLevel?: number
+}
+
+function Hello({ name, enthusiasmLevel = 1 }: Props) {
+  if (enthusiasmLevel <= 0) {
+    throw new Error('You cloud be a little more enthusiasmLevel. :D')
+  }
+  return (
+    <div className="hello">
+      <div className="greeting">
+        Hello {name + getExclamationMarks(enthusiasmLevel)}
+      </div>
+    </div>
+  )
+}
+
+export default Hello
+
+function getExclamationMarks(numChars: number) {
+  return Array(numChars + 1).join('!')
+}
